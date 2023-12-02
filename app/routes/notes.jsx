008a2b3b9f6c35e0c6abce39f28f3a497cb747b4
Josh <joshua.winters@tsymmetry.com>
2023-12-02 00:53:56 -0500

    this commit is tricky. this is tthe first foray into the backend, and here we set up a number of things in the action function on notes.jsx. we are getting the submitted form data, converting it, storing it, then appending it to the notes.json file, and redirecting to the same page after. this has been confirmed to work in the console via network, where the form returned a 200, then a 302 with no errors

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,5 +1,7 @@
+import { redirect } from '@remix-run/node';
 import NewNote, { links as NewNoteLinks} from '../components/NewNote';
 import newNoteStyles from '../components/NewNote.css';
+import { getStoredNotes, storedNotes } from '../data/notes';
 
 export default function NotesPage(){
     return (
@@ -9,8 +11,19 @@ export default function NotesPage(){
     )
 }
 
-export function action(){
-    
+export async function action({request}){
+    const formData = await request.formData();
+    // const noteData = {
+    //     title: formData.get('title'),
+    //     content: formData.get('content')
+    // };
+
+    const noteData = Object.fromEntries(formData);
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
+    const updatedNotes = existingNotes.concat(noteData);
+    await storedNotes(updatedNotes);
+    return redirect('/notes')
 }
 
 export function links() {
