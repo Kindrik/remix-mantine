a851455c6db4ce18322d9f7bd2643efcd8bb617a
Josh <joshua.winters@tsymmetry.com>
2023-12-01 21:27:37 -0500

    This commit is about NavLink, and how it applies an active class to the current link, as oppsed to just using Link

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import MainNavigation from './components/MainNavigation';
 import styles from './styles/main.css';
 
 // export const links: LinksFunction = () => [
@@ -25,6 +26,9 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
